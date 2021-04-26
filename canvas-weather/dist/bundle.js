435fb1e8c57734052c515c510d096c242c8205d7
geek720 <geek720s@gmail.com>
2021-04-26 17:31:35 +0800

    ok

--- canvas-weather/dist/bundle.js
+++ canvas-weather/dist/bundle.js
@@ -20,46 +20,95 @@ function _createClass(Constructor, protoProps, staticProps) {
   return Constructor;
 }
 
-var WindMill = /*#__PURE__*/function () {
-  function WindMill(opts) {
-    _classCallCheck(this, WindMill);
+function _toConsumableArray(arr) {
+  return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread();
+}
 
-    this.radius = 20;
-    this.x = opts.x;
-    this.y = opts.y;
-    this.ctx = opts.ctx;
-    this.initBase();
+function _arrayWithoutHoles(arr) {
+  if (Array.isArray(arr)) return _arrayLikeToArray(arr);
+}
+
+function _iterableToArray(iter) {
+  if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter);
+}
+
+function _unsupportedIterableToArray(o, minLen) {
+  if (!o) return;
+  if (typeof o === "string") return _arrayLikeToArray(o, minLen);
+  var n = Object.prototype.toString.call(o).slice(8, -1);
+  if (n === "Object" && o.constructor) n = o.constructor.name;
+  if (n === "Map" || n === "Set") return Array.from(o);
+  if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);
+}
+
+function _arrayLikeToArray(arr, len) {
+  if (len == null || len > arr.length) len = arr.length;
+
+  for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
+
+  return arr2;
+}
+
+function _nonIterableSpread() {
+  throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+}
+
+var FanBlade = /*#__PURE__*/function () {
+  function FanBlade(ctx, x, y, radius, angle, rotate) {
+    _classCallCheck(this, FanBlade);
+
+    this.x = x;
+    this.y = y;
+    this.rotate = rotate;
+    this.f1 = Math.cos(angle / 2) * radius;
+    this.f2 = Math.sin(angle / 2) * radius;
+    this.baserStartPoint = [this.f1, this.f2];
+    this.baserEndPoint = [this.f1, -this.f2];
+    this.baseControlPoint = [2 * this.f1, 0];
+    this.generalFan(ctx);
   }
 
-  _createClass(WindMill, [{
-    key: "initBase",
-    value: function initBase() {
-      // this.ctx.beginPath();
-      // this.ctx.strokeStyle = '#ffffff';
-      //
-      // this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-      // this.ctx.fillStyle = '#ffffff';
-      // this.ctx.stroke();
-      var ctx = this.ctx;
+  _createClass(FanBlade, [{
+    key: "generalFan",
+    value: function generalFan(ctx) {
+      ctx.save();
+      ctx.translate(this.x, this.y);
+      ctx.rotate(this.rotate);
+      this.drawQuad(ctx);
+      ctx.lineTo(0, 0);
+      ctx.lineTo.apply(ctx, _toConsumableArray(this.baserStartPoint));
+      ctx.fillStyle = '#fff';
+      ctx.fill();
+      ctx.restore();
+    }
+  }, {
+    key: "drawQuad",
+    value: function drawQuad(ctx) {
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = '#fff';
       ctx.beginPath();
-      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-      ctx.fillStyle = '#ffffff';
-      ctx.fill(); // this.ctx.fillStyle = "rgb(200,0,0)";
-      // this.ctx.fillRect (40, 40, 55, 50);
+      ctx.moveTo.apply(ctx, _toConsumableArray(this.baserStartPoint));
+      ctx.quadraticCurveTo(this.baseControlPoint[0], this.baseControlPoint[1], this.baserEndPoint[0], this.baserEndPoint[1]);
+      ctx.stroke();
     }
   }]);
 
-  return WindMill;
+  return FanBlade;
 }();
 
-var initCanvas = function initCanvas() {
-  var ctx = document.getElementById('canvas').getContext('2d');
-  new WindMill({
-    x: 40,
-    y: 40,
-    ctx: ctx
-  });
+var ctx = document.getElementById('canvas').getContext('2d');
+var lengthStep = 10;
+
+var draw = function draw() {
+  ctx.clearRect(0, 0, 800, 800);
+  ctx.translate(85, 80);
+  ctx.rotate(lengthStep / 180 * Math.PI);
+  ctx.translate(-85, -80);
+  new FanBlade(ctx, 85, 80, 40, 0.1 * Math.PI, -1 / 2 * Math.PI);
+  new FanBlade(ctx, 85, 80, 40, 0.1 * Math.PI, 30 / 180 * Math.PI);
+  new FanBlade(ctx, 85, 80, 40, 0.1 * Math.PI, 150 / 180 * Math.PI);
+  window.requestAnimationFrame(draw);
 };
 
-initCanvas();
+window.requestAnimationFrame(draw);
 //# sourceMappingURL=bundle.js.map
--- canvas-weather/src/main.js
+++ canvas-weather/src/main.js
@@ -1,41 +1,74 @@
 class FanBlade {
-    constructor() {
+    constructor(ctx, x, y, radius, angle, rotate) {
+        this.x = x;
+        this.y = y;
+        this.rotate = rotate;
+        this.f1 = Math.cos(angle / 2) * radius;
+        this.f2 = Math.sin(angle / 2) * radius;
+        this.baserStartPoint = [this.f1, this.f2];
+        this.baserEndPoint = [this.f1, -this.f2];
+        this.baseControlPoint = [2 * this.f1, 0];
+        this.generalFan(ctx);
+
+    }
+    generalFan(ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y)
+        ctx.rotate(this.rotate)
+        this.drawQuad(ctx);
+        ctx.lineTo(0 , 0);
+        ctx.lineTo(...this.baserStartPoint);
+        ctx.fillStyle = '#fff';
+        ctx.fill();
+        ctx.restore();
+
+    }
+    drawQuad(ctx) {
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = '#fff';
+        ctx.beginPath();
+        ctx.moveTo(...this.baserStartPoint);
+        ctx.quadraticCurveTo(this.baseControlPoint[0], this.baseControlPoint[1], this.baserEndPoint[0], this.baserEndPoint[1]);
+        ctx.stroke();
     }
 }
 
 class WindMill {
 
     constructor(opts) {
-        this.radius = 20;
+        this.radius = 5;
         this.x = opts.x;
         this.y = opts.y;
         this.ctx = opts.ctx;
         this.initBase();
     }
     initBase() {
-        // this.ctx.beginPath();
-        // this.ctx.strokeStyle = '#ffffff';
-        //
-        // this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-        // this.ctx.fillStyle = '#ffffff';
-        // this.ctx.stroke();
-
-
         const { ctx } = this;
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         ctx.fillStyle = '#ffffff';
         ctx.fill();
-
-        // this.ctx.fillStyle = "rgb(200,0,0)";
-        // this.ctx.fillRect (40, 40, 55, 50);
     }
 
+
 }
 
-const initCanvas = () => {
-    const ctx = document.getElementById('canvas').getContext('2d');
-    const windMill = new WindMill({ x: 40, y: 40, ctx });
+const ctx = document.getElementById('canvas').getContext('2d');
+const lengthStep = 10;
+
+const draw = () => {
+    ctx.clearRect(0,0,800,800);
+    ctx.translate(85, 80);
+    ctx.rotate((lengthStep / 180) * Math.PI);
+    ctx.translate(-85, -80);
+    const fanBlade1 = new FanBlade(ctx, 85, 80, 40, 0.1 * Math.PI,  ((-1 / 2) * Math.PI));
+    const fanBlade2 = new FanBlade(ctx, 85, 80, 40, 0.1 * Math.PI, (30 / 180) * Math.PI);
+    const fanBlade3 = new FanBlade(ctx, 85, 80, 40, 0.1 * Math.PI,   (150 / 180) * Math.PI);
+    window.requestAnimationFrame(draw);
 }
 
-initCanvas();
+window.requestAnimationFrame(draw);
+
+
+
+
